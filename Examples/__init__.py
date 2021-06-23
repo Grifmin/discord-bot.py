d96530628586c306a4956f94c82e0e0c64653602
Grifmin <61306689+Grifmin@users.noreply.github.com>
2021-06-23 09:16:40 -0400

    Formatted better, prevented the bot from processing commands (Will see why later)

--- Examples/__init__.py
+++ Examples/__init__.py
@@ -2,20 +2,32 @@
 This will help load in each cog
 '''
 from datetime import datetime
-from .events import *  # must import '.filename'
-from .Loader import * 
-from .main import * 
-from .moderation import * 
+from . import events   # must import '.filename'
+from . import owner
+from . import commands
+
 
 def load(client):
     '''Will help setup cogs'''
+    client.strip_after_prefix = True  # just a nice little touch.
+
+    # sets the clients uptime for other commands to beable to access.
     client.launch_time = datetime.utcnow()
 
-    client.load_extension(Loader.__name__)
-    client.load_extension(main.__name__)
-    client.load_extension(moderation.__name__)
+    @client.event
+    async def on_message(message) -> None:
+        '''
+        This funtion overrides the bot events,
+        preventing the bot from processing commmands.
+        '''
+        return
+
+    client.load_extension(owner.__name__)
     client.load_extension(events.__name__)
-    # To load your own extension, uncomment the following try block. Then put in your filename (without a '.' this time).
+    client.load_extension(commands.__name__)
+
+    # To load your own extension, uncomment the following try block.
+    # Then put in your filename (without a '.' this time).
     # try:
     #     client.load_extension(REPLACE ME.__name__)
     # except Exception as e:
